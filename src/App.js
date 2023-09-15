08d6b0859710205765c25b58d768baee74e148e3
PRITOM DUTTA <duttapritom777@gmail.com>
2023-09-15 23:16:56 +0530

    Add form in Add new section and provide style, set router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import AddMovie from "./components/AddMovie";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div >
       <Header/>
-      <Cards/>
+      <Routes>
+        <Route path="/" element={<Cards/>}/>
+        <Route path="/addmovie" element={<AddMovie/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button';
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
-        <span>Filmy<span className='text-white'>World</span></span>
-        <h1 className='text-lg flex items-center cursor-pointer'>
-          <Button><AddIcon className='mr-1' color='success'/> <span className='text-white'>Add New</span></Button>
+    <div className="text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500">
+      <span>
+        Filmy<span className="text-white">World</span>
+      </span>
+      <Link to={'/addmovie'}>
+        <h1 className="text-lg flex items-center cursor-pointer">
+          <Button>
+            <AddIcon className="mr-1" color="success" />{" "}
+            <span className="text-white">Add New</span>
+          </Button>
         </h1>
-        
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
