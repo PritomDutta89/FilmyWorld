5de014df8510d2d88095bc15c56753d5cdc89ab9
PRITOM DUTTA <duttapritom777@gmail.com>
2023-09-26 01:39:54 +0530

    Update UI

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -39,7 +39,7 @@ const Cards = () => {
                 key={index}
                 className="card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500"
               >
-                <img className="h-60 md:h-72 w-90 md:w-1" src={element.image} />
+                <img className="h-60 md:h-72 w-30 " width={180}  src={element.image} />
                 <h1>{element.title}</h1>
                 <h1 className="flex items-center">
                   <span className="text-gray-500 mr-1">Rating:</span>
